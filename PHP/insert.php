bb95647d3dc9dcd3bf145406470f54251cb7f163
Maksim Dotskin <124581027+DotskinMaksim@users.noreply.github.com>
2024-11-27 00:34:32 +0200

    Fix

--- PHP/insert.php
+++ PHP/insert.php
@@ -125,8 +125,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             <option value="0">0</option>
             <option value="1">1</option>
             <option value="2">2</option>
-            <option value="3">2</option>
-            <option value="4">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
         </select>
 
         <label for="cup_type">Cup type:</label>
